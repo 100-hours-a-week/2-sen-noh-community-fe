b8b8affc715a3ce9c74d7a9965b25951e6ae0747
sein <si2238@naver.com>
2024-10-31 23:56:45 +0900

    feat: addPost
    게시글 작성 html,css,js 추가

---
+++ addPost.js
@@ -0,0 +1,26 @@
+const title = document.getElementById("titleTextArea");
+const content = document.getElementById("contentTextArea");
+const editFinBtn = document.getElementById("editFinBtn");
+
+title.addEventListener("input", checkInput);
+content.addEventListener("input", checkInput);
+
+function checkInput() {
+  if (title.value !== "" && content.value !== "") {
+    editFinBtn.style.backgroundColor = "#7f6aee";
+  } else {
+    editFinBtn.style.backgroundColor = "#aca0eb"; // 기본 색으로 되돌리기
+  }
+}
+
+const helpText = document.getElementsByClassName("helpText")[0];
+
+editFinBtn.addEventListener("click", () => {
+  if (title.value == "" || content.value == "") {
+    helpText.textContent = "*제목, 내용을 모두 작성해주세요";
+    helpText.style.color = "#ff0000";
+  } else {
+    helpText.style.color = "#f4f5f7";
+  }
+  console.log("h");
+});
