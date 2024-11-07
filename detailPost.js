51cb731fad8b7abadd874fcb7085e34f8ae41169
sein <si2238@naver.com>
2024-11-07 14:24:40 +0900

    feat: link detailPost comment json data
    게시글상세조회 - 댓글 json 파일 연결

--- detailPost.js
+++ detailPost.js
@@ -1,78 +1,113 @@
-const backBtn = document.getElementById("back");
+const backBtn = document.getElementById('back');
 
-backBtn.addEventListener("click", () => {
-  history.back();
+backBtn.addEventListener('click', () => {
+    history.back();
 });
 
-const postId = new URLSearchParams(window.location.search).get("postId");
+const postId = new URLSearchParams(window.location.search).get('postId');
 
-const editBtn = document.getElementById("postEditBtn");
+const editBtn = document.getElementById('postEditBtn');
 
 fetch(`posts.json`)
-  .then((response) => response.json())
-  .then((posts) => {
-    data = posts[`${postId}` - 1];
-    document.getElementById("title").textContent = `${data.title}`;
-    document.getElementById("writerText").textContent = `${data.nickname}`;
-    document.getElementById("writeDate").textContent =
-      `${formatDates(data.date)}`;
-    document.getElementById("contentText").textContent = `${data.content}`;
-    document.getElementsByClassName("nums")[0].textContent =
-      `${formatLikes(data.heart_cnt)}`;
-    document.getElementsByClassName("nums")[2].textContent =
-      `${formatLikes(data.visit_cnt)}`;
-    document.getElementsByClassName("nums")[1].textContent =
-      `${formatLikes(data.chat_cnt)}`;
-    editBtn.addEventListener("click", () => {
-      const imgSrc = `${data.post_image}`;
-      const fileName = imgSrc.substring(imgSrc.lastIndexOf("/") + 1);
-      document.location.href = `editPost.html?title=${encodeURIComponent(data.title)}&body=${encodeURIComponent(data.content)}&img=${fileName}`;
-    });
-  })
-  .catch((error) => console.error("Fetch 오류:", error));
+    .then(response => response.json())
+    .then(posts => {
+        data = posts[`${postId}` - 1];
+        document.getElementById('title').textContent = `${data.title}`;
+        document.getElementById('writerText').textContent = `${data.nickname}`;
+        document.getElementById('writeDate').textContent =
+            `${formatDates(data.date)}`;
+        document.getElementById('contentText').textContent = `${data.content}`;
+        document.getElementsByClassName('nums')[0].textContent =
+            `${formatLikes(data.heart_cnt)}`;
+        document.getElementsByClassName('nums')[2].textContent =
+            `${formatLikes(data.visit_cnt)}`;
+        document.getElementsByClassName('nums')[1].textContent =
+            `${formatLikes(data.chat_cnt)}`;
+        editBtn.addEventListener('click', () => {
+            const imgSrc = `${data.post_image}`;
+            const fileName = imgSrc.substring(imgSrc.lastIndexOf('/') + 1);
+            document.location.href = `editPost.html?title=${encodeURIComponent(data.title)}&body=${encodeURIComponent(data.content)}&img=${fileName}`;
+        });
+    })
+    .catch(error => console.error('Fetch 오류:', error));
 
 function formatLikes(likes) {
-  if (likes >= 1000) {
-    return Math.floor(likes / 100) / 10 + "k";
-  } else {
-    return likes.toString();
-  }
+    if (likes >= 1000) {
+        return Math.floor(likes / 100) / 10 + 'k';
+    } else {
+        return likes.toString();
+    }
 }
 
 function formatDates(date) {
-  const d = new Date(date);
-  return d.toISOString().slice(0, 19).replace("T", " ");
+    const d = new Date(date);
+    return d.toISOString().slice(0, 19).replace('T', ' ');
 }
 
-const modal = document.getElementsByClassName("modalContainer")[0];
+const commentList = document.getElementById('commentList');
 
-document.getElementById("postDelBtn").addEventListener("click", () => {
-  modal.style.visibility = "visible";
-  document.body.style.overflow = "hidden";
+fetch('comments.json')
+    .then(res => res.json())
+    .then(data => {
+        comments = data.filter(cmt => cmt.post_id == postId);
+        comments.forEach(cmt => {
+            const cmtArticle = document.createElement('article');
+            cmtArticle.classList.add('introTitle2');
+            cmtArticle.classList.add('chatContainer');
+            cmtArticle.innerHTML = `
+            <div class="introTitle2">
+              <img src="./images/IMG_1533.JPG" class="writerImg" />
+              <div class="content">
+                <div class="chatTop">
+                  <p id="writerText" style="background-color: #f4f5f7">
+                    더미 작성자1
+                  </p>
+                  <p class="contentSub backTrans" style="margin-left: 24px">
+                    2021-01-01 00:00:00
+                  </p>
+                </div>
+                <p id="chatText">댓글내용</p>
+              </div>
+            </div>
+            <div style="margin-top: 19px">
+              <button class="editBtn">수정</button>
+              <button class="editBtn">삭제</button>
+            </div>
+      `;
+            commentList.appendChild(cmtArticle);
+        });
+    })
+    .catch(error => console.error('Fetch 오류:', error));
+
+const modal = document.getElementsByClassName('modalContainer')[0];
+
+document.getElementById('postDelBtn').addEventListener('click', () => {
+    modal.style.visibility = 'visible';
+    document.body.style.overflow = 'hidden';
 });
 
 document
-  .getElementsByClassName("modalBtnNo")[0]
-  .addEventListener("click", () => {
-    modal.style.visibility = "hidden";
-    document.body.style.overflow = "auto";
-  });
+    .getElementsByClassName('modalBtnNo')[0]
+    .addEventListener('click', () => {
+        modal.style.visibility = 'hidden';
+        document.body.style.overflow = 'auto';
+    });
 
 document
-  .getElementsByClassName("modalBtnYes")[0]
-  .addEventListener("click", () => {
-    modal.style.visibility = "hidden";
-    document.body.style.overflow = "auto";
-    document.location.href = "postList.html";
-  });
+    .getElementsByClassName('modalBtnYes')[0]
+    .addEventListener('click', () => {
+        modal.style.visibility = 'hidden';
+        document.body.style.overflow = 'auto';
+        document.location.href = 'postList.html';
+    });
 
-const cmtInput = document.getElementById("chatInput");
-const cmtBtn = document.getElementById("addCmtBtn");
+const cmtInput = document.getElementById('chatInput');
+const cmtBtn = document.getElementById('addCmtBtn');
 
-cmtInput.addEventListener("input", () => {
-  if (cmtInput.value.trim() != "") {
-    cmtBtn.style.backgroundColor = "#7f6aee";
-  } else {
-    cmtBtn.style.backgroundColor = "#aca0eb";
-  }
+cmtInput.addEventListener('input', () => {
+    if (cmtInput.value.trim() != '') {
+        cmtBtn.style.backgroundColor = '#7f6aee';
+    } else {
+        cmtBtn.style.backgroundColor = '#aca0eb';
+    }
 });
--- postList.js
+++ postList.js
@@ -1,21 +1,21 @@
-const addBtn = document.getElementById("addBtn");
+const addBtn = document.getElementById('addBtn');
 
-addBtn.addEventListener("click", () => {
-  document.location.href = "addPost.html";
+addBtn.addEventListener('click', () => {
+    document.location.href = 'addPost.html';
 });
 
-const postList = document.getElementById("postList");
+const postList = document.getElementById('postList');
 
 let posts = [];
 
-fetch("posts.json")
-  .then((response) => response.json())
-  .then((data) => {
-    posts = data; // 데이터 할당
-    posts.forEach((post) => {
-      const postArticle = document.createElement("article");
-      postArticle.classList.add("post");
-      postArticle.innerHTML = `
+fetch('posts.json')
+    .then(response => response.json())
+    .then(data => {
+        posts = data; // 데이터 할당
+        posts.forEach(post => {
+            const postArticle = document.createElement('article');
+            postArticle.classList.add('post');
+            postArticle.innerHTML = `
         <div class="contentTitle">${post.title.length > 26 ? post.title.slice(0, 26) : post.title}</div>
         <div class="contentSubTotal">
           <div class="contentSubNum">
@@ -26,25 +26,30 @@ fetch("posts.json")
             <p class="contentSub">조회수&nbsp;</p>
             <p class="contentSub">${formatLikes(post.visit_cnt)}&nbsp;&nbsp;</p>
           </div>
-          <p class="contentSub" style="padding-right: 24px">${post.date}</p>
+          <p class="contentSub" style="padding-right: 24px">${formatDates(post.date)}</p>
         </div>
         <div class="writer">
           <img src="./images/IMG_1533.JPG" class="writerImg" />
           <p id="writerText">${post.nickname}</p>
         </div>
       `;
-      postList.appendChild(postArticle);
-      postArticle.addEventListener("click", () => {
-        document.location.href = `detailPost.html?postId=${post.post_id}`;
-      });
-    });
-  })
-  .catch((error) => console.error("Fetch 오류:", error));
+            postList.appendChild(postArticle);
+            postArticle.addEventListener('click', () => {
+                document.location.href = `detailPost.html?postId=${post.post_id}`;
+            });
+        });
+    })
+    .catch(error => console.error('Fetch 오류:', error));
 
 function formatLikes(likes) {
-  if (likes >= 1000) {
-    return Math.floor(likes / 100) / 10 + "k";
-  } else {
-    return likes.toString();
-  }
+    if (likes >= 1000) {
+        return Math.floor(likes / 100) / 10 + 'k';
+    } else {
+        return likes.toString();
+    }
+}
+
+function formatDates(date) {
+    const d = new Date(date);
+    return d.toISOString().slice(0, 19).replace('T', ' ');
 }
