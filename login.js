e6aa9765bf15ae15650a6f28fc884b3da68ae392
sein <si2238@naver.com>
2024-11-18 12:04:21 +0900

    feat: add login api
    로그인 api 연결

--- login.js
+++ login.js
@@ -2,15 +2,40 @@ const inputs = document.getElementsByClassName('emailInput');
 const loginBtn = document.getElementById('loginBtn');
 const helpText = document.getElementById('helpText');
 
-const pw = 'Aaaa123@';
-loginBtn.addEventListener('click', () => {
-    const text = outputText();
-    if (text != '') {
-        helpText.textContent = text;
+let emailText;
+let pwText;
+
+inputs[0].addEventListener('blur', () => {
+    emailText = outputEmailText();
+    if (emailText !== '') {
+        helpText.textContent = emailText;
         helpText.style.visibility = 'visible';
-    } else {
+        loginBtn.style.backgroundColor = '#ACA0EB';
+    } else if (pwText === '') {
         loginBtn.style.backgroundColor = '#7f6aee';
-        document.location.href = 'postList.html';
+        helpText.style.visibility = 'hidden';
+    }
+});
+
+inputs[1].addEventListener('blur', () => {
+    pwText = outputPWText();
+    if (pwText !== '') {
+        helpText.textContent = pwText;
+        helpText.style.visibility = 'visible';
+        loginBtn.style.backgroundColor = '#ACA0EB';
+    } else if (emailText === '') {
+        loginBtn.style.backgroundColor = '#7f6aee';
+        helpText.style.visibility = 'hidden';
+    }
+});
+
+loginBtn.addEventListener('click', () => {
+    if (pwText === '' && emailText === '') {
+        const data = {
+            email: inputs[0].value,
+            password: inputs[1].value,
+        };
+        login(data);
     }
 });
 
@@ -18,24 +43,25 @@ document.getElementById('signInText').onclick = function () {
     document.location.href = 'signIn.html';
 };
 
-function outputText() {
+function outputEmailText() {
     if (!validEmail(inputs[0].value)) {
         return '* 올바른 이메일 주소 형식을 입력해주세요. (예: example@example.com)';
     }
-    if (inputs[1].value == '') {
+    return '';
+}
+
+function outputPWText() {
+    if (inputs[1].value === '') {
         return '* 비밀번호를 입력해주세요.';
     }
     if (!validPW(inputs[1].value)) {
         return '* 비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
     }
-    if (inputs[1].value != pw) {
-        return '* 비밀번호가 다릅니다.';
-    }
     return '';
 }
 
 function validEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
 }
 
@@ -43,4 +69,20 @@ function validPW(pw) {
     const regex =
         /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,20}$/;
     return regex.test(pw);
+}
+
+function login(data) {
+    axios
+        .post('http://localhost:3000/auth/login', data)
+        .then(res => {
+            helpText.style.visibility = 'hidden';
+            if (res.status === 201) {
+                document.location.href = 'postList.html';
+            }
+        })
+        .catch(err => {
+            loginBtn.style.backgroundColor = '#ACA0EB';
+            helpText.textContent = '* ' + err.response.data.message;
+            helpText.style.visibility = 'visible';
+        });
 }
