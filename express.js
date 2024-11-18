ed4f3b0b441d3aa00046e9cfee8c22ce63aac62d
sein <si2238@naver.com>
2024-11-18 12:03:38 +0900

    chore: add axios, prettier
    axios, prettier 추가

--- express.js
+++ express.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
+    res.sendFile(join(__dirname, 'login.html'));
 });
 
 app.get('/signIn', (req, res) => {
-    res.sendFile(path.join(__dirname, 'signIn.html'));
+    res.sendFile(join(__dirname, 'signIn.html'));
 });
 
 app.get('/postList', (req, res) => {
-    res.sendFile(path.join(__dirname, 'postList.html'));
+    res.sendFile(join(__dirname, 'postList.html'));
 });
 
 app.listen(port, () => {
