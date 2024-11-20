0945ae438fcb5ceec80a825caf7d4f7f8a9782fa
sein <si2238@naver.com>
2024-11-20 11:46:48 +0900

    feat: add checkNickname api
    닉네임중복 api 연결
    회원가입 및 회원정보 수정에 연결

--- editProfile.js
+++ editProfile.js
@@ -3,12 +3,15 @@ const editBtn = document.getElementById('loginBtn');
 const helpText = document.getElementsByClassName('helpText')[0];
 const toast = document.getElementById('editFinBtn');
 
-editBtn.addEventListener('click', () => {
+editBtn.addEventListener('click', async () => {
     if (nickname.value === '') {
         helpText.textContent = '*닉네임을 입력해주세요.';
         helpText.style.visibility = 'visible';
     } else if (nickname.value.length > 11) {
         helpText.textContent = '*닉네임은 최대 10자 까지 작성 가능합니다.';
+        helpText.style.visibility = 'visible';
+    } else if (await existNickname({ nickname: nickname.value })) {
+        helpText.textContent = '*중복된 닉네임 입니다.';
         helpText.style.visibility = 'visible';
     } else {
         const data = {
@@ -76,4 +79,17 @@ function deleteUser() {
         .catch(err => {
             console.error(err);
         });
+}
+
+async function existNickname(data) {
+    try {
+        const res = await axios.post(
+            'http://localhost:3000/auth/checkNickname',
+            data,
+        );
+        return res.data.data.is_existed; // 서버에서 받은 값을 반환
+    } catch (err) {
+        console.error(err);
+        return true; // 기본값으로 true 반환 (중복된 이메일로 처리)
+    }
 }
--- signIn.js
+++ signIn.js
@@ -51,9 +51,7 @@ async function emailHelp(email) {
     if (!validEmail(email)) {
         return '*올바른 이메일 주소 형식을 입력해주세요. (예: example@example.com)';
     }
-    const isExisted = await existEmail({ email });
-    if (isExisted) {
-        console.log('중복');
+    if (await existEmail({ email })) {
         return '*중복된 이메일 입니다. ';
     }
     return '';
@@ -71,9 +69,6 @@ function pwHelp(pw) {
     if (!validPW(pw)) {
         return '*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
     }
-    //   if (pw != inputs[2].value) {
-    //     return "*비밀번호가 다릅니다. ";
-    //   }
     return '';
 }
 
@@ -93,20 +88,18 @@ function rePwHelp(pw) {
     return '';
 }
 
-const names = 'sen';
-
-function nickNameHelp(name) {
+async function nickNameHelp(name) {
     if (name === '') {
         return '*닉네임을 입력해주세요.';
     }
     if (/\s/.test(name)) {
         return '*띄어쓰기를 없애주세요';
     }
-    if (name === names) {
-        return '*중복된 닉네임입니다.';
-    }
     if (name.length > 10) {
         return '*닉네임은 최대 10자 까지 작성 가능합니다.';
+    }
+    if (await existNickname({ nickname: name })) {
+        return '*중복된 닉네임입니다.';
     }
     return '';
 }
@@ -126,6 +119,19 @@ async function existEmail(data) {
     try {
         const res = await axios.post(
             'http://localhost:3000/auth/checkEmail',
+            data,
+        );
+        return res.data.data.is_existed; // 서버에서 받은 값을 반환
+    } catch (err) {
+        console.error(err);
+        return true; // 기본값으로 true 반환 (중복된 이메일로 처리)
+    }
+}
+
+async function existNickname(data) {
+    try {
+        const res = await axios.post(
+            'http://localhost:3000/auth/checkNickname',
             data,
         );
         return res.data.data.is_existed; // 서버에서 받은 값을 반환
