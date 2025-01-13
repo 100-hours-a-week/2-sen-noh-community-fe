e79083403d33d3fd42e9d80cbc42bd8537308b0e
sein <si2238@naver.com>
2025-01-13 14:56:31 +0900

    feat: redesign editPW page
    비밀번호 수정 페이지 디자인 수정

--- public/js/user/editPW.js
+++ public/js/user/editPW.js
@@ -7,27 +7,31 @@ const finBtn = document.getElementById('loginBtn');
 const toast = document.getElementById('editFinBtn');
 let isSuccess = true;
 
+const root = document.documentElement;
+const darkOrange = getComputedStyle(root).getPropertyValue('--dark-orange');
+const orange = getComputedStyle(root).getPropertyValue('--orange');
+
 inputs[0].addEventListener('blur', () => {
     if (inputs[0].value === '') {
         helpPW.style.visibility = 'visible';
         helpPW.textContent = '*비밀번호를 입력해주세요.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else if (!validPW(inputs[0].value)) {
         helpPW.style.visibility = 'visible';
         helpPW.textContent =
             '*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else if (inputs[0].value !== inputs[1].value) {
         helpPW.style.visibility = 'visible';
         helpPW.textContent = '*비밀번호 확인과 다릅니다.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else {
         helpPW.style.visibility = 'hidden';
         helpRePW.style.visibility = 'hidden';
-        finBtn.style.backgroundColor = '#7f6aee';
+        finBtn.style.backgroundColor = darkOrange;
         isSuccess = true;
     }
 });
@@ -36,23 +40,23 @@ inputs[1].addEventListener('blur', () => {
     if (inputs[1].value === '') {
         helpRePW.style.visibility = 'visible';
         helpRePW.textContent = '*비밀번호를 입력해주세요.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else if (inputs[0].value !== inputs[1].value) {
         helpRePW.style.visibility = 'visible';
         helpRePW.textContent = '*비밀번호 확인과 다릅니다.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else if (!validPW(inputs[0].value)) {
         helpPW.style.visibility = 'visible';
         helpPW.textContent =
             '*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 최소 1개 포함해야 합니다.';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         isSuccess = false;
     } else {
         helpPW.style.visibility = 'hidden';
         helpRePW.style.visibility = 'hidden';
-        finBtn.style.backgroundColor = '#7f6aee';
+        finBtn.style.backgroundColor = darkOrange;
         isSuccess = true;
     }
 });
@@ -74,7 +78,7 @@ async function updatePW(data) {
         await api.patch(`/users/password`, data);
         inputs[0].value = '';
         inputs[1].value = '';
-        finBtn.style.backgroundColor = '#aca0eb';
+        finBtn.style.backgroundColor = orange;
         toast.style.visibility = 'visible';
         setTimeout(() => {
             toast.style.visibility = 'hidden';
