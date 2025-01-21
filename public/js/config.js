9496a0d576ba8fd4cc3bc8573f5187ef24fa901a
sein <si2238@naver.com>
2025-01-21 15:59:25 +0900

    feat: change server url

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://13.124.136.185:3000/api';
+export const SERVER_URL = 'http://13.124.136.185/api/';
 export const IMG_URL = 'https://d1x1wryfq8jc0f.cloudfront.net/';
